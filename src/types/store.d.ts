d1fdafb74e537cb5116de46947b63e9201f1ba35
青衫依旧 <2461521018@qq.com>
2022-03-11 23:30:32 +0800

    change

--- src/types/store.d.ts
+++ src/types/store.d.ts
@@ -4,16 +4,7 @@
  * @Autor: qsyj
  * @Date: 2022-03-10 22:57:39
  * @LastEditors: qsyj
- * @LastEditTime: 2022-03-10 23:53:39
- * @FilePath: \vite-admin-vue\src\types\store.d.ts
- */
-/*
- * @Description:
- * @Version: 1.0.0
- * @Autor: qsyj
- * @Date: 2022-03-10 22:57:39
- * @LastEditors: qsyj
- * @LastEditTime: 2022-03-10 23:02:56
+ * @LastEditTime: 2022-03-11 23:28:03
  * @FilePath: \vite-admin-vue\src\types\store.d.ts
  */
 
