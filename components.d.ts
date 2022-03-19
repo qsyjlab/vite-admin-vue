0aceffa371e3c16b8b12819f7fa190c72f1f7c37
青衫依旧 <2461521018@qq.com>
2022-03-20 00:13:17 +0800

    husky 提交

--- components.d.ts
+++ components.d.ts
@@ -23,4 +23,4 @@ declare module 'vue' {
   }
 }
 
-export { }
+export {}
--- src/hooks/session/BaseCookie.ts
+++ src/hooks/session/BaseCookie.ts
@@ -21,17 +21,14 @@ export default class BaseCookie {
     const Days = 30
     const exp = new Date()
     exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000)
-    document.cookie =
-      name + '=' + encodeURIComponent(value) + ';expires=' + exp.toUTCString()
+    document.cookie = name + '=' + encodeURIComponent(value) + ';expires=' + exp.toUTCString()
   }
   /**
    * 获取cookie
    * @param {*} cookie_name
    */
   get(name: string) {
-    const arr = document.cookie.match(
-      new RegExp('(^| )' + name + '=([^;]*)(;|$)')
-    )
+    const arr = document.cookie.match(new RegExp('(^| )' + name + '=([^;]*)(;|$)'))
     if (arr != null) return decodeURIComponent(arr[2])
     return false
   }
