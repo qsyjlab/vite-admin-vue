a7347f09cc1f4837d8cf341682c9e8ac28ad84b7
青衫依旧 <2461521018@qq.com>
2022-11-08 14:00:36 +0800

    feat(hooks): useElementCssVar

--- src/hooks/layout-config/use-theme-color.ts
+++ src/hooks/layout-config/use-theme-color.ts
@@ -1,18 +1,39 @@
-/**
- * 更新 element primary 配色
- * @param colorList
- */
-export const updateElementCssVar = (colorList: string[]): void => {
-  // document.documentElement is global
+import { generateThemeCluster } from '@/utils'
+
+export const useElementCssVar = () => {
   const el = document.documentElement
-  // set css var
-  el.style.setProperty('--el-color-primary', colorList[0])
-  el.style.setProperty('--el-button-bg-color', colorList[0])
-  el.style.setProperty('--el-button-active-bg-color', colorList[10])
-  el.style.setProperty('--el-button-active-border-color', colorList[10])
-  el.style.setProperty('--el-color-primary-dark-2', colorList[1])
-  el.style.setProperty('--el-color-primary-dark-1', colorList[1])
-  for (let index = 1; index <= 9; index++) {
-    el.style.setProperty(`--el-color-primary-light-${index}`, colorList[index])
+
+  const style = el.style
+
+  const setElementCssVar = (color: string): void => {
+    let colorList: string[] = []
+
+    colorList = generateThemeCluster(color)
+    // set css var
+    style.setProperty('--el-color-primary', colorList[0])
+    style.setProperty('--el-button-bg-color', colorList[0])
+    style.setProperty('--el-button-active-bg-color', colorList[10])
+    style.setProperty('--el-button-active-border-color', colorList[10])
+    style.setProperty('--el-color-primary-dark-2', colorList[1])
+    style.setProperty('--el-color-primary-dark-1', colorList[1])
+    for (let index = 1; index <= 9; index++) {
+      style.setProperty(`--el-color-primary-light-${index}`, colorList[index])
+    }
+  }
+  const removeElementCssVar = () => {
+    style.removeProperty('--el-color-primary')
+    style.removeProperty('--el-button-bg-color')
+    style.removeProperty('--el-button-active-bg-color')
+    style.removeProperty('--el-button-active-border-color')
+    style.removeProperty('--el-color-primary-dark-2')
+    style.removeProperty('--el-color-primary-dark-1')
+    for (let index = 1; index <= 9; index++) {
+      style.removeProperty(`--el-color-primary-light-${index}`)
+    }
+  }
+
+  return {
+    setElementCssVar,
+    removeElementCssVar
   }
 }
