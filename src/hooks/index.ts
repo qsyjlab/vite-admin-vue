62ba2dfd9f54fafe362d3f7177e039d23fed8afe
青衫依旧 <2461521018@qq.com>
2023-08-09 17:28:25 +0800

    feat: hooks useMutateObserver

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -7,6 +7,7 @@ export { useRouterHelper, useStorageHelper }
 export * from './layout-config'
 
 export { useContext } from './core/use-context'
+export { useMutateObserver } from './core/use-mutate-observer'
 
 export { useProForm } from '@/components/pro-form/src/use-pro-form'
 export * from './use-watermark'
