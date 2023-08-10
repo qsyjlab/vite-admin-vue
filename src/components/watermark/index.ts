955ded589da5bc8342e974d4409a6f68459533bf
青衫依旧 <2461521018@qq.com>
2023-08-10 09:32:26 +0800

    feat: 添加 watermark 类型导出

--- src/components/watermark/index.ts
+++ src/components/watermark/index.ts
@@ -4,3 +4,4 @@ import WatermarkComponent from './src/watermark.vue'
 export const Watermark = withInstall(WatermarkComponent)
 
 export { useWaterMark } from './src/use-watermark'
+export type { WatermarkProps } from './src/use-watermark'
