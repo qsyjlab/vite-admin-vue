c98f9c5011e80fa65ed3f4dfc05ae0158acf9d4a
青衫依旧 <2461521018@qq.com>
2023-08-16 15:05:30 +0800

    fix: 删除不符合 hooks 不分, 部分以迁移至 utils

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,3 @@
-import { useRouterHelper } from './router'
-
-import { useStorageHelper } from './session'
-
-export { useRouterHelper, useStorageHelper }
-
 export * from './layout-config'
 
 export { useContext } from './core/use-context'
