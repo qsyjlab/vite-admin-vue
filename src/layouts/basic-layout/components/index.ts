b211eab4e513c5a8bfeaf1943c10f01bdd858371
青衫依旧 <2461521018@qq.com>
2023-08-21 10:01:09 +0800

    fix: 修复 tab-page 路径

--- src/layouts/basic-layout/components/index.ts
+++ src/layouts/basic-layout/components/index.ts
@@ -1,7 +1,7 @@
 import BasicHeader from './header/basic-header.vue'
 import BasicSetting from './setting/setting-draver.vue'
 import BasicFooter from './footer/footer.vue'
-import BasicTabPage from './router-bar/router-bar.vue'
+import BasicTabPage from './tab-page/tab-page.vue'
 import BasicSidebar from './sidebar/sidebar.vue'
 import { MixSidebar as BasicMixSidebar } from './mix-sidebar'
 
