293f74564e49eb9987236780ce0ea30b1014e68f
青衫依旧 <2461521018@qq.com>
2023-08-22 17:03:04 +0800

    fix: 修复面包屑导航

--- src/layouts/basic-layout/components/header/components/Breadcrumb/Breadcrumb.vue
+++ src/layouts/basic-layout/components/header/components/Breadcrumb/Breadcrumb.vue
@@ -1,79 +1,25 @@
 <template>
   <el-breadcrumb :separator="'/'">
-    <el-breadcrumb-item v-for="item in breadCrumbList" :key="item.path" :to="{ name: item.name }">{{
+    <el-breadcrumb-item v-for="item in matched" :key="item.path" :to="{ name: item.name }">{{
       item?.meta?.title
     }}</el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script setup lang="ts">
-import { usePermissionStore } from '@/store'
-import { computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref, onUnmounted } from 'vue'
+import { RouteRecordNormalized } from 'vue-router'
+import { routeChangeListener } from '@/router'
 
-const { currentRoute } = useRouter()
-const { getMenus } = usePermissionStore()
+const matched = ref<RouteRecordNormalized[]>([])
 
-const breadCrumbList = computed(() => {
-  const menus = getMenus()
-
-  let path = currentRoute.value.path
-
-  const parent = getAllParentPath(menus, path)
-
-  const filterMenus = menus.filter(item => item.path === parent[0])
-  const matched = getMatched(filterMenus, parent) as any
-
-  return matched.filter((i: any) => !i.meta.hideInBreadcrumb)
+const stopListener = routeChangeListener((to, from, _matched) => {
+  matched.value = _matched
 })
 
-function getAllParentPath<T = any>(treeData: T[], path: string) {
-  const menuList = findPath(treeData, (n: any) => n.path === path)
-
-  return (menuList || []).map((item: any) => item.path)
-}
-
-function findPath<T = any>(tree: any, func: any): T | T[] | null {
-  const config = {
-    children: 'children'
-  }
-  const path: T[] = []
-  const list = [...tree]
-  const visitedSet = new Set()
-  const { children } = config
-  while (list.length) {
-    const node = list[0]
-    if (visitedSet.has(node)) {
-      path.pop()
-      list.shift()
-    } else {
-      visitedSet.add(node)
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      node[children!] && list.unshift(...node[children!])
-      path.push(node)
-      if (func(node)) {
-        return path
-      }
-    }
-  }
-  return null
-}
-
-function getMatched(menus: any[], parent: string[]) {
-  const metched: any[] = []
-  menus.forEach(item => {
-    if (parent.includes(item.path)) {
-      metched.push({
-        ...item,
-        name: item.name
-      })
-    }
-    if (item.children?.length) {
-      metched.push(...getMatched(item.children, parent))
-    }
-  })
-  return metched
-}
+onUnmounted(() => {
+  stopListener()
+})
 </script>
 
 <style lang="scss" scoped>
