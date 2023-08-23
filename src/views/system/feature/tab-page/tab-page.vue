cf43bf57c32d2025fef9cb618a60a624c75b652f
青衫依旧 <2461521018@qq.com>
2023-08-23 23:30:34 +0800

    feat: tab-page demo

--- src/views/system/feature/tab-page/tab-page.vue
+++ src/views/system/feature/tab-page/tab-page.vue
@@ -1,24 +1,23 @@
 <template>
-  <div>
-    <el-card>
-      <div>tab 栏</div>
-
+  <div style="height: 100%; box-sizing: border-box; padding: 10px">
+    <page-card header="tab 栏" style="margin-bottom: 20px">
       <el-input v-model="title"></el-input>
 
       <el-button type="primary" @click="setTitle(title)">设置标题</el-button>
-    </el-card>
-    <el-card :header="'打开新的标签页'">
+    </page-card>
+    <page-card :header="'打开新的标签页'">
       <el-button
         type="primary"
         @click="$router.push({ name: 'TabPageDetail', params: { id: new Date().getTime() } })"
         >打开新标签</el-button
       >
-    </el-card>
+    </page-card>
   </div>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useTabs } from '@/hooks'
+import { PageCard } from '@/components/page-card'
 
 const { setTitle } = useTabs()
 
