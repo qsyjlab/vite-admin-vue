3381fb2a4f4806225040bf0db5f3cb5f1469a5d5
青衫依旧 <2461521018@qq.com>
2023-08-23 23:30:21 +0800

    feat(components): page-card

---
+++ src/components/page-card/src/page-card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page-card">
+    <div class="page-card__header">
+      <div class="card-header">
+        <slot name="header">{{ header }}</slot>
+      </div>
+    </div>
+    <div class="page-card__body">
+      <slot name="default"></slot>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import './page-card.scss'
+
+withDefaults(
+  defineProps<{
+    header?: string
+  }>(),
+  {}
+)
+
+defineSlots<{
+  header?: () => void
+  default?: () => void
+}>()
+</script>
