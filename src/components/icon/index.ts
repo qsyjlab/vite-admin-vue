1e504a3ea545ca1eae713468b548cac4b0455cc8
青衫依旧 <2461521018@qq.com>
2023-08-24 16:57:36 +0800

    feat: @iconify 组件集成

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,3 +1,10 @@
+import { withInstall } from '@/utils'
 import Density from './src/density.vue'
 
 export { Density }
+
+import IconSelectorComponent from './src/icon-selector.vue'
+
+export const IconSelector = withInstall(IconSelectorComponent)
+
+export default IconSelector
---
+++ src/components/icon/src/icon-selector.vue
@@ -0,0 +1,36 @@
+<template>
+  <Icon v-if="iconType === 'ify'" :icon="iconName" :color="color" :width="size" :height="size" />
+</template>
+<script lang="ts">
+export default { name: 'IconSelector' }
+</script>
+<script setup lang="ts">
+import { watch, ref } from 'vue'
+import { Icon } from '@iconify/vue'
+
+const props = withDefaults(
+  defineProps<{
+    icon?: string
+    size?: number
+    color?: string
+  }>(),
+  {
+    size: 24
+  }
+)
+
+const iconType = ref('')
+const iconName = ref('')
+
+watch(
+  () => ref(props),
+  () => {
+    const nSplit = props.icon?.split('.')
+    iconType.value = nSplit?.[0] || ''
+    iconName.value = nSplit?.[1] || ''
+  },
+  {
+    immediate: true
+  }
+)
+</script>
