257fc4a6016e2833bc5e81cebbe526e960bc23c3
青衫依旧 <2461521018@qq.com>
2023-08-24 21:46:45 +0800

    feat: page-wrapper

---
+++ src/components/page-wrapper/src/page-wrapper.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="['page-wrapper', full ? 'is-full' : '']">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'PageWrapper'
+})
+
+defineSlots<{
+  default: () => void
+}>()
+
+withDefaults(
+  defineProps<{
+    full?: boolean
+  }>(),
+  {
+    full: true
+  }
+)
+</script>
+<style lang="scss" scoped>
+.page-wrapper {
+  padding: 10px;
+  &.is-full {
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+  }
+}
+</style>
