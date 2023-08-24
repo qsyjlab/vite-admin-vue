baf7dd35dec6d3f3081e6515a56d03f0919773a3
青衫依旧 <2461521018@qq.com>
2023-08-24 10:00:22 +0800

    chore: visualizer

--- build/vite/index.ts
+++ build/vite/index.ts
@@ -21,7 +21,7 @@ export function createVitePlugin(configEnv: ConfigEnv) {
       gzipSize: true,
       brotliSize: true,
       emitFile: false,
-      filename: 'test.html', //分析图生成的文件名
+      filename: '.visualizer.html', //分析图生成的文件名
       open: true //如果存在本地服务端口，将在打包后自动展示
     })
   ]
