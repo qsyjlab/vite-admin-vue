6665126aa94e080186b3bff7e25bb4b6235dc900
青衫依旧 <2461521018@qq.com>
2023-08-25 17:12:37 +0800

    fix: remove icon demo

--- src/router/routes/modules/icon.ts
+++ src/router/routes/modules/icon.ts
@@ -1,29 +1,4 @@
 import { defineExposeRoutes } from '@/router'
 import { Layout } from '@/router/constant'
 
-export default defineExposeRoutes([
-  {
-    path: '/icon',
-    name: 'Icon',
-    meta: {
-      title: 'Icon 图标',
-      sort: 6,
-
-      icon: 'ep.folder'
-    },
-    component: Layout,
-    children: [
-      {
-        path: '/',
-        name: 'IconIndex',
-        meta: {
-          title: 'Icon 图标',
-          sort: 6,
-
-          icon: 'ep.folder'
-        },
-        component: () => import(/* webpackChunkName: "Welcome" */ '@/views/system/icon/Icon.vue')
-      }
-    ]
-  }
-])
+export default defineExposeRoutes([])
