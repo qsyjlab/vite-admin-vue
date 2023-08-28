98225a42ed2770eeaff5a19dd009367a9191bc99
青衫依旧 <2461521018@qq.com>
2023-08-28 11:14:36 +0800

    feat: utils type 类型

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -3,22 +3,28 @@ export const toTypeString = (value: unknown): string => Object.prototype.toStrin
 export const toRawType = (value: unknown): string => {
   return toTypeString(value).slice(8, -1)
 }
-export const isObject = (val: unknown): val is Record<any, any> =>
-  val !== null && typeof val === 'object'
+export const isObject = (val: unknown): val is object => val !== null && typeof val === 'object'
 
-export const isPlainObject = (value: unknown) => toTypeString(value) === '[object Object]'
+export const isPlainObject = (value: unknown): value is Record<string | number | symbol, unknown> =>
+  toTypeString(value) === '[object Object]'
 export const isArray = Array.isArray
-export const isDate = (value: unknown) => toTypeString(value) === '[object Date]'
-export const isString = (value: unknown) => toTypeString(value) === '[object String]'
-export const isUndefined = (value: unknown) => toTypeString(value) === '[object Undefined]'
-export const isNull = (value: unknown) => toTypeString(value) === '[object Null]'
-export const isSymbol = (value: unknown) => toTypeString(value) === '[object Symbol]'
-export const isMap = (value: unknown) => toTypeString(value) === '[object Map]'
-export const isSet = (value: unknown) => toTypeString(value) === '[object Set]'
+export const isDate = (value: unknown): value is Date => toTypeString(value) === '[object Date]'
+export const isString = (value: unknown): value is string =>
+  toTypeString(value) === '[object String]'
+export const isUndefined = (value: unknown): value is undefined =>
+  toTypeString(value) === '[object Undefined]'
+export const isNull = (value: unknown): value is null => toTypeString(value) === '[object Null]'
+export const isSymbol = (value: unknown): value is symbol =>
+  toTypeString(value) === '[object Symbol]'
+export const isMap = (value: unknown): value is Map<unknown, unknown> =>
+  toTypeString(value) === '[object Map]'
+export const isSet = (value: unknown): value is Set<unknown> =>
+  toTypeString(value) === '[object Set]'
 export const isFunction = (value: unknown) => toTypeString(value) === '[object Function]'
 export const isBoolean = (value: unknown) => toTypeString(value) === '[object Boolean]'
-export const isRegExp = (value: unknown) => toTypeString(value) === '[object RegExp]'
-export const toString = (value: unknown) => toTypeString(value)
+export const isRegExp = (value: unknown): value is RegExp =>
+  toTypeString(value) === '[object RegExp]'
+
 export const objectToString = Object.prototype.toString
 
 export const extend = Object.assign
