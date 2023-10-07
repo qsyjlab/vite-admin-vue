65dc22885be38f3d7705191aa35fa8680d0c4a00
青衫依旧 <2461521018@qq.com>
2023-10-07 22:14:53 +0800

    feat: 调整 provider

--- src/application/src/app-provider.vue
+++ src/application/src/app-provider.vue
@@ -1,10 +1,14 @@
 <script lang="ts">
-import { computed, defineComponent, ref, readonly } from 'vue'
+import { computed, defineComponent, ref, readonly, h } from 'vue'
 import { useTitle } from '@vueuse/core'
 import { useRoute } from 'vue-router'
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 
 import { createBreakpointListener } from '@/hooks/event/use-breakpoint'
 import { createAppProviderContext } from './context'
+import { getLayoutCache } from '@/store/local'
+import { useLayoutConfigHandler } from '@/hooks'
 
 import projectConfig from '@/config/project-setting'
 import config from '@/config'
@@ -16,6 +20,8 @@ export default defineComponent({
     const route = useRoute()
 
     const isMobile = ref(false)
+
+    initAppConfig()
 
     createBreakpointListener(({ screenMap, sizeEnum, width }) => {
       const lgWidth = screenMap.get(sizeEnum.XS)
@@ -35,7 +41,12 @@ export default defineComponent({
       })
     )
 
-    return () => slots.default?.()
+    function initAppConfig() {
+      const { initLayout } = useLayoutConfigHandler()
+      initLayout(getLayoutCache() || {})
+    }
+
+    return () => h(ElConfigProvider, { locale: zhCn }, () => slots.default?.())
   }
 })
 </script>
