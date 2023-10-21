c4a51cdfd46d50cf1efd5c9a31a68e5d5edece13
青衫依旧 <2461521018@qq.com>
2023-10-22 00:32:29 +0800

    build:  buildComponents

--- build/scripts/buildComponents.ts
+++ build/scripts/buildComponents.ts
@@ -3,6 +3,9 @@ const { build } = require('vite')
 
 const vue = require('@vitejs/plugin-vue')
 const vueJsx = require('@vitejs/plugin-vue-jsx')
+
+const dts = require('vite-plugin-dts')
+
 const projectRootPath = process?.cwd() || resolve(__dirname, '..', '..')
 
 async function buildComponents() {
@@ -13,6 +16,7 @@ async function buildComponents() {
 
     build: {
       copyPublicDir: false,
+
       outDir: resolve(projectRootPath, 'docs', '.vitepress', '.exampleCompnents'),
       lib: {
         entry: resolve(projectRootPath, 'src', 'components'),
@@ -22,10 +26,29 @@ async function buildComponents() {
       rollupOptions: {
         // 配置 external
         external: ['vue', '@element-plus/icons-vue', 'element-plus']
-      }
+      },
+      output: [
+        {
+          format: 'es',
+          //不用打包成.es.js,这里我们想把它打包成.js
+          entryFileNames: '[name].js',
+          //让打包目录和我们目录对应
+          preserveModules: true,
+          //配置打包根目录
+          dir: 'es'
+          // preserveModulesRoot: 'src'
+        }
+      ]
     },
 
-    plugins: [vue(), vueJsx()],
+    plugins: [
+      vue(),
+      vueJsx(),
+      dts({
+        entryRoot: resolve(projectRootPath, 'src', 'components'),
+        tsConfigFilePath: resolve(projectRootPath, 'tsconfig.json')
+      })
+    ],
 
     resolve: {
       alias: {
