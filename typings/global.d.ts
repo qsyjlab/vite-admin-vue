32dc50683c3c82678d497b4f2e99e2c1e638c998
青衫依旧 <2461521018@qq.com>
2023-10-26 16:29:43 +0800

    feat: DeepPartial

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -5,3 +5,7 @@ declare type NOOP = () => void
 declare interface Fn<T = any, R = T> {
   (...arg: T[]): R
 }
+
+declare type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P]
+}
