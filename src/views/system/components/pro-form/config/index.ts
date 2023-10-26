9173a9c0c6228b6c3f8c97a3efba9443f9edf107
青衫依旧 <2461521018@qq.com>
2023-10-26 20:31:35 +0800

    fix: useSchema

---
+++ src/views/system/components/pro-form/config/index.ts
@@ -0,0 +1,18 @@
+export const fields = [
+  {
+    label: '测试',
+    key: 'test',
+    attrs: {
+      options: [],
+      disabled: true
+    }
+  },
+  {
+    label: '测试2',
+    key: 'test2',
+    attrs: {
+      options: [],
+      disabled: true
+    }
+  }
+]
