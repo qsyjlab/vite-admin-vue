a3b1903bc45ee84a302269e66fc5ab76bb52ed70
青衫依旧 <2461521018@qq.com>
2023-11-17 11:22:39 +0800

    fix: 修改部分默认样式

--- components.d.ts
+++ components.d.ts
@@ -42,6 +42,7 @@ declare module 'vue' {
     ElRow: typeof import('element-plus/es')['ElRow']
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     ElSpace: typeof import('element-plus/es')['ElSpace']
+    ElStatistic: typeof import('element-plus/es')['ElStatistic']
     ElStep: typeof import('element-plus/es')['ElStep']
     ElSteps: typeof import('element-plus/es')['ElSteps']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
