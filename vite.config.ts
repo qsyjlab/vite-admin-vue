cfeba9df8820a16162c74af2b00d4d46d40ef350
青衫依旧 <2461521018@qq.com>
2024-03-07 18:16:19 +0800

    chore: vite5 迁移

--- vite.config.ts
+++ vite.config.ts
@@ -34,50 +34,7 @@ export default defineConfig(configEnv => {
       proxy: createProxy(viteEnvs.SERVER_PROXY_LIST)
     },
     build: {
-      outDir: resolveProjectPath(buildOutdir, `dist_${mode}`),
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            tinymce: [
-              'tinymce',
-              'tinymce/icons/default',
-              'tinymce/plugins/advlist',
-              'tinymce/plugins/anchor',
-              'tinymce/plugins/autolink',
-              'tinymce/plugins/autosave',
-              'tinymce/plugins/code',
-              'tinymce/plugins/codesample',
-              'tinymce/plugins/directionality',
-              'tinymce/plugins/fullscreen',
-              'tinymce/plugins/hr',
-              'tinymce/plugins/insertdatetime',
-              'tinymce/plugins/link',
-              'tinymce/plugins/lists',
-              'tinymce/plugins/media',
-              'tinymce/plugins/nonbreaking',
-              'tinymce/plugins/noneditable',
-              'tinymce/plugins/pagebreak',
-              'tinymce/plugins/paste',
-              'tinymce/plugins/preview',
-              'tinymce/plugins/print',
-              'tinymce/plugins/save',
-              'tinymce/plugins/searchreplace',
-              'tinymce/plugins/spellchecker',
-              'tinymce/plugins/tabfocus',
-              'tinymce/plugins/table',
-              'tinymce/plugins/template',
-              'tinymce/plugins/textpattern',
-              'tinymce/plugins/visualblocks',
-              'tinymce/plugins/visualchars',
-              'tinymce/plugins/wordcount',
-              'tinymce/plugins/image',
-              'tinymce/themes/silver',
-              'tinymce/themes/mobile'
-            ],
-            echarts: ['echarts']
-          }
-        }
-      }
+      outDir: resolveProjectPath(buildOutdir, `dist_${mode}`)
     }
   }
 })
