f36f7832766c3813b429fd76714b3b2b26014157
青衫依旧 <2461521018@qq.com>
2024-03-11 18:01:55 +0800

    feat: setting 按钮组

--- src/layouts/basic-layout/components/setting/setting-draver.vue
+++ src/layouts/basic-layout/components/setting/setting-draver.vue
@@ -1,85 +1,93 @@
 <template>
   <el-drawer v-model="isOpenSettig" :with-header="false" direction="rtl" size="370px">
     <!-- {{ layoutConfig }} -->
-    <div class="setting-body">
+    <div class="setting">
       <div class="setting-title">系统布局配置</div>
+      <div class="setting-body">
+        <div class="setting-item">
+          <div class="setting-item__label">主题色</div>
+          <div class="setting-item__content">
+            <el-color-picker
+              v-model="layoutConfig.themeColor"
+              @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.THEME_COLOR, value)"
+            />
+          </div>
+        </div>
 
-      <div class="setting-item">
-        <div class="setting-item__label">主题色</div>
-        <div class="setting-item__content">
-          <el-color-picker
-            v-model="layoutConfig.themeColor"
-            @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.THEME_COLOR, value)"
-          />
+        <div class="setting-item is-vertical">
+          <div class="setting-item__label">导航模式</div>
+          <div class="setting-item__content">
+            <CheckButtonGroup
+              :default-value="layoutConfig.layoutMode"
+              :options="layoutModeOptions"
+              @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.LAYOUT_MODE, value)"
+            />
+          </div>
+        </div>
+
+        <div class="setting-item">
+          <div class="setting-item__label">折叠菜单</div>
+          <div class="setting-item__content">
+            <el-switch
+              v-model="layoutConfig.collapsed"
+              @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.COLLAPSED, value)"
+            />
+          </div>
+        </div>
+
+        <div class="setting-item">
+          <div class="setting-item__label">是否分割菜单</div>
+          <div class="setting-item__content">
+            <el-switch
+              v-model="layoutConfig.splitMenu"
+              :disabled="LayoutMode.TopMix !== layoutConfig.layoutMode"
+              @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.SPLIT_MENU, value)"
+            />
+          </div>
+        </div>
+
+        <div class="setting-item">
+          <div class="setting-item__label">菜单展开宽度</div>
+          <div class="setting-item__content">
+            <el-input-number
+              v-model="layoutConfig.asideWidth"
+              @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.MENU_WIDTH, value)"
+            />
+          </div>
+        </div>
+
+        <div class="setting-item">
+          <div class="setting-item__label">头部高度</div>
+          <div class="setting-item__content">
+            <el-input-number
+              v-model="layoutConfig.headerHeight"
+              @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.HEADER_HEIGHT, value)"
+            />
+          </div>
+        </div>
+
+        <div class="setting-item">
+          <div class="setting-item__label">tab bar高度</div>
+          <div class="setting-item__content">
+            <el-input-number
+              v-model="layoutConfig.tabBarHeight"
+              @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.TAB_BAR_HEIGHT, value)"
+            />
+          </div>
         </div>
       </div>
-
-      <div class="setting-item is-vertical">
-        <div class="setting-item__label">导航模式</div>
-        <div class="setting-item__content">
-          <CheckButtonGroup
-            :default-value="layoutConfig.layoutMode"
-            :options="layoutModeOptions"
-            @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.LAYOUT_MODE, value)"
-          />
-        </div>
-      </div>
-
-      <div class="setting-item">
-        <div class="setting-item__label">折叠菜单</div>
-        <div class="setting-item__content">
-          <el-switch
-            v-model="layoutConfig.collapsed"
-            @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.COLLAPSED, value)"
-          />
-        </div>
-      </div>
-
-      <div class="setting-item">
-        <div class="setting-item__label">是否分割菜单</div>
-        <div class="setting-item__content">
-          <el-switch
-            v-model="layoutConfig.splitMenu"
-            :disabled="LayoutMode.TopMix !== layoutConfig.layoutMode"
-            @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.SPLIT_MENU, value)"
-          />
-        </div>
-      </div>
-
-      <div class="setting-item">
-        <div class="setting-item__label">菜单展开宽度</div>
-        <div class="setting-item__content">
-          <el-input-number
-            v-model="layoutConfig.asideWidth"
-            @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.MENU_WIDTH, value)"
-          />
-        </div>
-      </div>
-
-      <div class="setting-item">
-        <div class="setting-item__label">头部高度</div>
-        <div class="setting-item__content">
-          <el-input-number
-            v-model="layoutConfig.headerHeight"
-            @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.HEADER_HEIGHT, value)"
-          />
-        </div>
-      </div>
-
-      <div class="setting-item">
-        <div class="setting-item__label">tab bar高度</div>
-        <div class="setting-item__content">
-          <el-input-number
-            v-model="layoutConfig.tabBarHeight"
-            @change="(value: any) => setLayoutConfig(LayoutConfigHandlerEnum.TAB_BAR_HEIGHT, value)"
-          />
-        </div>
+      <div class="setting-footer">
+        <el-space style="width: 100%" fill direction="vertical">
+          <el-button :icon="CopyDocument" type="primary">复制</el-button>
+          <el-button :icon="Refresh" type="warning">重置</el-button>
+        </el-space>
       </div>
     </div>
   </el-drawer>
 </template>
 
 <script setup lang="ts">
+import { CopyDocument, Refresh } from '@element-plus/icons-vue'
 import { useLayoutStore } from '@/store'
 import { storeToRefs } from 'pinia'
 import { LayoutMode } from '../../enum'
@@ -118,15 +126,11 @@ const layoutModeOptions = [
 </script>
 
 <style lang="scss" scoped>
-.setting-container {
-  position: fixed;
-  z-index: 1004;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  background-color: white;
-
-  /* background: rgba(0, 0, 0, 0.2); */
+.setting {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  min-height: 0;
 }
 
 .setting-body {
@@ -137,10 +141,7 @@ const layoutModeOptions = [
   box-sizing: border-box;
 }
 
-.setting-bottom {
-  position: absolute;
-  top: 40%;
-  transform: translateY(-50%);
+.setting-footer {
 }
 
 .setting-title {
